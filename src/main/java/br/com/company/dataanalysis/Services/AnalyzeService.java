e1dc75567106b2b14e89c65bc8d0fe8ffbf235be
cassio <cassioribeiropereira@gmail.com>
2020-07-30 19:20:45 -0300

    feat(AnalyzerService): implementar restante classe de analise de dados.

--- src/main/java/br/com/company/dataanalysis/Services/AnalyzeService.java
+++ src/main/java/br/com/company/dataanalysis/Services/AnalyzeService.java
@@ -1,6 +1,9 @@
 package br.com.company.dataanalysis.Services;
 
+import br.com.company.dataanalysis.Entities.Client;
+import br.com.company.dataanalysis.Entities.Sale;
 import br.com.company.dataanalysis.Entities.Salesman;
+import br.com.company.dataanalysis.Utils.Filter;
 import org.springframework.stereotype.Service;
 
 import java.io.*;
@@ -19,49 +22,52 @@ public class AnalyzeService {
     SaleService saleService = new SaleService();
     SalesmanService salesmanService = new SalesmanService();
     ClientService clientService = new ClientService();
+    List<Object> objects = new ArrayList<>();
+
+    Filter filter = new Filter();
 
     public void analyzer(List<File> files) {
-        List<Objects> objects = new ArrayList<>();
-        for (File file : files){
+        List<File> filtered = filter.filter(files);
+        for (File file : filtered){
             System.out.println(file.getAbsoluteFile());
             int wichLine = 1;
             reader(file, wichLine);
         }
-
     }
 
-    public List<Object> reader(File file, Integer wichLine){
-        List<Object> objs = new ArrayList<>();
+    public void reader(File file, Integer wichLine){
         try {
             List<String> lines = Files.readAllLines(file.toPath());
             for(String line: lines) {
-                readerLine(line, wichLine, objs, file);
+                readerLine(line, wichLine, file);
+                wichLine++;
             }
 
         }catch (Exception e){
             System.out.println("Erro: " + e.getMessage());
         }
-        System.out.println(objs.size());
-        return objs;
+        System.out.println(objects.size());
     }
 
-    public void readerLine(String line, Integer wichLine, List<Object> objs, File file){
+    public void readerLine(String line, Integer wichLine, File file){
         if(readerService.linesValidator(line)){
-            System.out.println("Linha valida " +line + wichLine);
-            wichLine++;
+            System.out.println("Valid line " + wichLine +" - "+ line );
             String cod = readerService.getCod(line);
             switch (cod) {
                 case "001" :
-                    objs.add(salesmanService.createSalesman(line, objs, wichLine, file));
+                    Salesman salesman = salesmanService.createSalesman(line, objects, wichLine, file);
+                    if(salesman != null) objects.add(salesman);
                     break;
                 case "002":
-                    objs.add(clientService.createClient(line, objs, wichLine, file));
+                    Client client = clientService.createClient(line, objects, wichLine, file);
+                    if(client != null) objects.add(client);
                     break;
                 case "003":
-
+                    Sale sale = saleService.createSale(line, objects, wichLine, file);
+                    if(sale != null) objects.add(sale);
                     break;
                 default:
-
+                    logger.info("something was not right");
                     break;
             }
         }else{
